d8af65b42af3c895283d977cb2517daa8aab1cd7
alesikivan <obcofficiall@gmail.com>
2026-03-02 23:02:51 +0300

    upd ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,7 +18,11 @@ module.exports = {
       repo: 'https://github.com/alesikivan/plants-api',
       path: '/root/apps/plants-backend',
 
-      'post-deploy': `npm install && npm run build && pm2 delete plants-backend && pm2 start ecosystem.config.js && pm2 save --force`
+      'post-deploy': `
+        export NVM_DIR="$HOME/.nvm"
+        [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+        npm install && npm run build && pm2 delete plants-backend && pm2 start ecosystem.config.js && pm2 save --force
+      `
     }
   }
 };
